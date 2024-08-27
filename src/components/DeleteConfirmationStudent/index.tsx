e1370e6de6c4bcb8ca77c3998359cd1a2846ced4
Rafael Menegalli <rafaelmenegalli@gmail.com>
2024-08-26 21:28:13 -0300

    a

--- src/components/DeleteConfirmationStudent/index.tsx
+++ src/components/DeleteConfirmationStudent/index.tsx
@@ -1,5 +1,8 @@
-import { useState } from "react";
+// import { useState } from "react";
+import styles from "./styles.module.scss";
+
 import { Modal, Button } from "rsuite";
+import RemindIcon from '@rsuite/icons/legacy/Remind';
 
 interface DeleteConfirmModalProps {
     open: () => void;
@@ -9,11 +12,10 @@ interface DeleteConfirmModalProps {
 export function DeleteConfirmationStudent({ open, visible }: DeleteConfirmModalProps) {
     return (
         <>
-            <Modal open={visible} onClose={open}>
-                <Modal.Header>Confirmar exclusão de aluno</Modal.Header>
-
-                <Modal.Body>
-                    Tem certeza que deseja excluir este item?
+            <Modal open={visible} onClose={open} role="alertdialog" size="xs">
+                <Modal.Body className={styles.modalBody}>
+                    <RemindIcon style={{ color: '#ff1100', fontSize: 24 }} />
+                    Tem certeza que deseja excluir este aluno?
                 </Modal.Body>
 
                 <Modal.Footer>
