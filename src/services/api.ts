d4645c14600599e1b590f66b221a3b856b72f068
Rafael Menegalli <rafaelmenegalli@gmail.com>
2024-10-24 17:01:08 -0300

    Ajustando API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import { GetServerSidePropsContext } from "next";
 import axios, { AxiosError } from "axios";
-import { parseCookies } from "nookies";
+import { parseCookies, destroyCookie } from "nookies";
 import { signOut } from "@/contexts/AuthContext";
 
 export function setupAPIClient(ctx?: GetServerSidePropsContext) {
@@ -8,28 +8,32 @@ export function setupAPIClient(ctx?: GetServerSidePropsContext) {
     const token = cookies['@mscauth.token'];
 
     const URLProd = "https://msc-api-production.up.railway.app";
-    const URLHomolog = "https://devoted-ambition-production.up.railway.app"
+    const URLHomolog = "https://devoted-ambition-production.up.railway.app";
 
     const api = axios.create({
         baseURL: URLProd,
         headers: {
             authorization: `Bearer ${token}`
         }
-    })
+    });
 
     api.interceptors.response.use((response) => {
-        return response
+        return response;
     }, (error: AxiosError) => {
         if (error.response?.status === 401) {
-            if (typeof window !== undefined) {
-                signOut();
-            } else {
-                return;
+            if (typeof window !== "undefined") {
+                // Caso o código esteja rodando no client-side
+                signOut(); // Desloga o usuário
+            } else if (ctx) {
+                // Caso o código esteja rodando no server-side (SSR)
+                destroyCookie(ctx, '@mscauth.token'); // Limpa o cookie com o token inválido
+                ctx.res.writeHead(302, { Location: "/" }); // Redireciona para a página de login
+                ctx.res.end();
             }
         }
 
-        return Promise.reject(error)
-    })
+        return Promise.reject(error);
+    });
 
     return api;
 }
